d4c60f117722b2d1131715bab758d3de0e8de70f
avanronaufaladi <avaroadi@gmail.com>
2024-05-28 12:08:59 +0700

    memanggil nilai saklar

--- pengapsulan/pengapsulan.cpp
+++ pengapsulan/pengapsulan.cpp
@@ -5,3 +5,16 @@ class remoteLampu
 {
 private:
 	string saklarNo[10];
+
+public:
+	//untuk mengisi nilai saklar
+	void setsaklarNo(int i, string value)
+	{
+		saklarNo[i] = value;
+	}
+	//untuk memasukkan nilai saklar
+	string getsaklarNo(int i)
+	{
+		return saklarNo[i];
+	}
+};
