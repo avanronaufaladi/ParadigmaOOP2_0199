033251324c386990ce0d9ff0d992b6c087b6dbee
avanronaufaladi <avaroadi@gmail.com>
2024-05-28 12:03:10 +0700

    memasukkan class lia

--- ParadigmaOOP2_0199.cpp
+++ ParadigmaOOP2_0199.cpp
@@ -13,4 +13,23 @@ public:
 	//}
 };
 
-}
+class joko : public seseorang
+{
+public:
+	//deklarasi
+	void pesan()
+	{
+		//implementasi
+		cout << "pesan dari joko" << endl;
+	}
+};
+
+class lia : public seseorang
+{
+public:
+	void pesan()
+	{
+		cout << "pesan dari lia" << endl;
+
+	}
+};
