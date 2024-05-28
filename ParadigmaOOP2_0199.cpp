44220455befc1106d7914112ab2700368114cd22
avanronaufaladi <avaroadi@gmail.com>
2024-05-28 12:02:16 +0700

    memasukkan virtual void pesan

--- ParadigmaOOP2_0199.cpp
+++ ParadigmaOOP2_0199.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class seseorang
+{
+public:
+	//pure virtual function
+	virtual void pesan() = 0;
+	//virtual function biasa
+	//virtual void pesan()
+	//{
+	// cout << "pesan dari seseorang" << endl;
+	//}
+};
+
+}
